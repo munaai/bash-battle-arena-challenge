7a25430dd2994e68712e5b4c460f89f6ac5c6cad
munaibrahimn <munaa97@gmail.com>
2025-04-01 18:28:00 +0100

    level

--- Level6.sh
+++ Level6.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+if [[ -z $1 ]]
+then  
+    echo "No file name provided"
+    exit 1
+fi
+
+
+if [[ ! -f $1 ]]
+then
+    echo "File does not exist"
+    exit 1
+fi
+
+file_found=$(wc -l < "$1")
+echo $file_found
+
+#if you want to run with an argument run ./Level6.sh example.txt
